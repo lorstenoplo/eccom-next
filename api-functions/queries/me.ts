1b6407daa18f9837ed0ceb9053c293c856e80934
nishanth <gayatridipali@gmail.com>
2021-04-18 10:47:00 +0530

    git delete working

--- api-functions/queries/me.ts
+++ api-functions/queries/me.ts
@@ -1,6 +1,7 @@
 import { AUTH_API_BASE_URL } from "../../utils/constants";
 
 export default async function me(token: string) {
+  console.log(token);
   const response = await fetch(`${AUTH_API_BASE_URL}/me/${token}`).then((res) =>
     res.json()
   );
--- pages/account.tsx
+++ pages/account.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import useStyles from "../mui-styles/Account_Styles";
 import Head from "next/head";
@@ -6,20 +6,62 @@ import Head from "next/head";
 import { Layout } from "../components";
 import { Button } from "@material-ui/core";
 import { useDeleteUserMutation } from "../src/generated/graphql";
+import { useMutation, useQuery } from "react-query";
+import { AUTH_API_BASE_URL } from "../utils/constants";
+import me from "../api-functions/queries/me";
 
 const account = () => {
   const classes = useStyles();
   // const [user] = useGetUser();
+  const [token, setToken] = useState<string>("");
 
-  const [{ data, error, fetching }, deleteUser] = useDeleteUserMutation();
+  useEffect(() => {
+    setToken(localStorage.getItem("qid") || "");
+  }, []);
 
+  const { data, isLoading, isError } = useQuery(
+    ["me", token],
+    async () => {
+      const data = await me(token!);
+      return data;
+    },
+    {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+    }
+  );
+
+  const values = {
+    username: data?.user.username,
+    email: data?.user.email,
+  };
+
+  // const [{ data, error, fetching }, deleteUser] = useDeleteUserMutation();
+  const mutation = useMutation(async (values: any) => {
+    const res = await fetch(`${AUTH_API_BASE_URL}/delete`, {
+      method: "POST",
+      body: JSON.stringify(values),
+    });
+
+    return res.json();
+  });
   return (
     <motion.div initial="initial" animate="animate" exit={{ opacity: 0 }}>
-      <Head>{/* <title>GoLoop Account | {user?.username}</title> */}</Head>
+      <Head>
+        {/* <title>GoLoop Account | {user?.username}</title> */}
+        GoLoop Account
+      </Head>
       <Layout navColor="#FAFAFA" className={classes.page}>
         {/* hi {user?.username} */}
-        <Button>Delete account</Button>
-        {/* {error && <p>{error.message}</p>} */}
+        <Button
+          onClick={async () => {
+            const res = await mutation.mutateAsync(values);
+            console.log("del >>>", res);
+          }}
+        >
+          Delete account
+        </Button>
+        {mutation.isError && <p>{(mutation.error as any).message}</p>}
       </Layout>
     </motion.div>
   );
