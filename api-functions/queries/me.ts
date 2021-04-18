9c04eeeaf9834221a0fe9035c52ef709368abf4b
nishanth <gayatridipali@gmail.com>
2021-04-18 10:51:02 +0530

    fixed no token case

--- api-functions/queries/me.ts
+++ api-functions/queries/me.ts
@@ -1,7 +1,10 @@
 import { AUTH_API_BASE_URL } from "../../utils/constants";
 
 export default async function me(token: string) {
-  console.log(token);
+  if (!token) {
+    return null;
+  }
+
   const response = await fetch(`${AUTH_API_BASE_URL}/me/${token}`).then((res) =>
     res.json()
   );
