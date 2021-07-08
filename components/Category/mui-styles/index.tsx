e223060471eb380edc817a07dd6762ccda9eb24b
Nishanth <69671750+lorstenoplo@users.noreply.github.com>
2021-07-08 16:07:19 +0530

    Update index.tsx

--- components/Category/mui-styles/index.tsx
+++ components/Category/mui-styles/index.tsx
@@ -8,6 +8,7 @@ const useStyles = makeStyles((theme: Theme) =>
       width: "100%",
       [theme.breakpoints.down("md")]: {
         height: 200,
+        objectFit:"unset",
       },
     },
   })
